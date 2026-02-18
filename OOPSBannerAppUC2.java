c16bd09f1caa8c8fde158b08126043d8d1a02524
av0434 <av0434@srmist.edu.in>
2026-02-18 10:11:54 +0530

    UC2 ASCII ART

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,11 @@
+public class OOPSAsciiArt {
+    public static void main(String[] args) {
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *            *");
+        System.out.println("*     * *     * *      *     *");
+        System.out.println(" *****   *****   *       ***** ");
+    }
+}
